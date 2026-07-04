ab4310fec20ff2117a14e507f7061f5300ef2018
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-07-04 21:16:52 +0100

    feat: add optional SteamId property to PlayerConnectedEvent class

--- src/XtremeIdiots.Portal.Server.Events.Abstractions.V1/Events/PlayerConnectedEvent.cs
+++ src/XtremeIdiots.Portal.Server.Events.Abstractions.V1/Events/PlayerConnectedEvent.cs
@@ -3,6 +3,7 @@ namespace XtremeIdiots.Portal.Server.Events.Abstractions.V1.Events;
 public class PlayerConnectedEvent : ServerEventBase
 {
     public required string PlayerGuid { get; init; }
+    public string? SteamId { get; init; }
     public required string Username { get; init; }
     public required string IpAddress { get; init; }
     public required int SlotId { get; init; }
